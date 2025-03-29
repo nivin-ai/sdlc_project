968d17f8a248286c653a5fdab47eaa2b6f858428
Nivin Sebastian <nivinsebastian@Nivins-MacBook-Air.local>
2025-03-29 13:25:07 +0530

    fixed a syntax error

--- src/sdlc_agenticai/nodes/user_story.py
+++ src/sdlc_agenticai/nodes/user_story.py
@@ -6,6 +6,6 @@ class UserStory:
         self.llm = llm
     def create_user_story(self, state: State)->dict:
         st.sidebar.write("generating user story...")
-        user_story =  self.llm.invoke(f"Create a user story for the following requirement: {state['user_requirement']}. Consider the following feedback to create better user stories: {state["po_review"]}.")
+        user_story =  self.llm.invoke(f"Create a user story for the following requirement: {state['user_requirement']}. Consider the following feedback to create better user stories: {state['po_review']}.")
         st.session_state["state"]["user_story"] = user_story.content
         return {"user_story": user_story.content}
