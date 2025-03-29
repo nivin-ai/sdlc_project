828dac4fd05295b3dafe1382323f4ecf0402c131
Nivin Sebastian <nivinsebastian@Nivins-MacBook-Air.local>
2025-03-29 13:28:31 +0530

    fixed syntax errors

--- src/sdlc_agenticai/nodes/design_documents.py
+++ src/sdlc_agenticai/nodes/design_documents.py
@@ -6,6 +6,6 @@ class DesignDocuments:
         self.llm = llm
     def generate_design_documents(self, state: State)->dict:
         st.sidebar.write("generating design documents...")
-        design_documents =  self.llm.invoke(f"Create really good, top notch, both functional and technical design documents based on the following. Requirement: {state["user_requirement"]}. User story: {state["user_story"]}.")
+        design_documents =  self.llm.invoke(f"Create really good, top notch, both functional and technical design documents based on the following. Requirement: {state['user_requirement']}. User story: {state['user_story']}.")
         st.session_state["state"]["design_documents"] = design_documents.content
         return {"design_documents": design_documents.content}
